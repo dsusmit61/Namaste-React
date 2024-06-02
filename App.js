7197dbbe79b7049cf99bf43556870e13807d6bc5
dsusmit61 <dsusmit61@gmail.com>
2024-06-02 10:27:23 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+ * <div id="parent">
+ *    <div id="child">
+ *       <h1>I am an H1 tag</h1>
+ *       <h2>I am an H2 tag</h2>
+ *    </div>
+ * </div>
+ */
+
+//How to create Nested structure inside React
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am an H1 tag"),
+//     React.createElement("h2", {}, "I am an H2 tag"),
+//   ])
+// );
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h2", {}, "This is an H2 of child1"),
+//     React.createElement("p", {}, "This is a paragraph of child1"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h2", {}, "This is an H2 of child2"),
+//     React.createElement("p", {}, "This is a paragraph of child2"),
+//   ]),
+// ]);
+
+const mainDiv = React.createElement(
+  "div",
+  { id: "main" },
+  React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h2", {}, "Heading2"),
+      React.createElement("p", {}, "Paragraph1"),
+    ]),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      React.createElement("p", { id: "para2" }, "Paragraph2")
+    ),
+  ])
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React Universe"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(mainDiv);
